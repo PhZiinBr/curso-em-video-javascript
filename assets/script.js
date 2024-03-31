616a903fa913e697951bd24486c867064eac7170
PhZiinBr <juhsantos.goleiro634@gmail.com>
2024-03-31 15:52:41 -0300

    implementando verificador por faixa de idade

--- assets/script.js
+++ assets/script.js
@@ -17,35 +17,42 @@ function verificar() {
             if (idade >= 0 && idade < 10) {
                 //Criança
                 imagem.setAttribute('src', 'assets/img/crianca_menino.jpeg')
+                res.innerHTML = `Detectamos um menino com ${idade} anos.`
             } else if (idade < 21) {
                 //Jovem
                 imagem.setAttribute('src', 'assets/img/jovem_homem.jpg')
+                res.innerHTML = `Detectamos um jovem com ${idade} anos.`
             } else if (idade < 50) {
                 //Adulto
                 imagem.setAttribute('src', 'assets/img/homem.jpeg')
+                res.innerHTML = `Detectamos um homem com ${idade} anos.`
             } else {
                 //Idoso
                 imagem.setAttribute('src', 'assets/img/senhor.jpeg')
+                res.innerHTML = `Detectamos um senhor com ${idade} anos.`
             }
         } else {
             gen = 'Feminino'
             if (idade >= 0 && idade < 10) {
                 //Criança
                 imagem.setAttribute('src', 'assets/img/crianca_menina.jpg')
+                res.innerHTML = `Detectamos uma menina com ${idade} anos.`
             } else if (idade < 21) {
                 //Jovem
                 imagem.setAttribute('src', 'assets/img/jovem_mulher.jpeg')
+                res.innerHTML = `Detectamos uma jovem com ${idade} anos.`
             } else if (idade < 50) {
                 //Adulto
                 imagem.setAttribute('src', 'assets/img/mulher.jpg')
+                res.innerHTML = `Detectamos uma mulher com ${idade} anos.`
             } else {
                 //Idoso
                 imagem.setAttribute('src', 'assets/img/senhora.jpeg')
+                res.innerHTML = `Detectamos uma senhora com ${idade} anos.`
             }
         }
 
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos gênero ${gen} com ${idade} anos.`
         res.appendChild(imagem)
     }
 }
